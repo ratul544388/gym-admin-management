2741e0564b1079daed68af0abd3a1118c0c3cc67
Ratul Hossain <ratulhosainbnc@gmamil.com>
2024-12-24 15:33:50 +0600

    fix-3

--- src/app/(site)/members/add-new/page.tsx
+++ src/app/(site)/members/add-new/page.tsx
@@ -4,7 +4,6 @@ import { MemberForm } from "../_components/member-form";
 import { Separator } from "@/components/ui/separator";
 import { redirect } from "next/navigation";
 
-export const dynamic = "force-dynamic";
 
 const AddNewMemberPage = async () => {
   const membershipPlans = await db.membershipPlan.findMany();
