227aedcd62cd51129ea5a2aeb8967ca2d0b3bda3
Ratul Hossain <ratulhosainbnc@gmamil.com>
2024-12-25 16:57:20 +0600

    fix-12

--- src/app/(site)/members/add-new/page.tsx
+++ src/app/(site)/members/add-new/page.tsx
@@ -1,13 +1,26 @@
 import { PageHeader } from "@/components/page-header";
 import { Separator } from "@/components/ui/separator";
-import Test from "./test";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { MemberForm } from "../_components/member-form";
 
-const AddNewMemberPage = () => {
+const AddNewMemberPage = async () => {
+  const membershipPlans = await db.membershipPlan.findMany();
+  const lockers = await db.locker.findMany({
+    where: {
+      member: null,
+    },
+  });
+
+  if (!membershipPlans.length) {
+    redirect("/membership-plans/new");
+  }
+  
   return (
     <div className="space-y-3">
       <PageHeader label="Add a new Member" showBackButton />
       <Separator />
-      <Test/>
+      <MemberForm membershipPlans={membershipPlans} lockers={lockers} />
     </div>
   );
 };
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
 declare global {
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const db = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+export { db };
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
