ced72db698b2570c777efd252f5e2e5d6add7ab4
Ratul Hossain <ratulhosainbnc@gmamil.com>
2025-04-02 23:54:19 +0600

    added search input for membership plans page

--- src/app/(main)/membership-plans/page.tsx
+++ src/app/(main)/membership-plans/page.tsx
@@ -1,9 +1,9 @@
+import { DataTable } from "@/components/data-table";
 import { PageHeader } from "@/components/page-header";
 import { db } from "@/lib/db";
-import { columns } from "./_components/table/columns";
-import { DataTable } from "@/components/data-table";
-import { Suspense } from "react";
 import { Metadata } from "next";
+import { columns } from "./_components/table/columns";
+import { SearchParamsType } from "@/types";
 
 export const generateMetadata = (): Metadata => {
   return {
@@ -11,14 +11,34 @@ export const generateMetadata = (): Metadata => {
   };
 };
 
-const MembershipPlansPage = async () => {
-  const memebershipPlans = await db.membershipPlan.findMany();
+const MembershipPlansPage = async ({
+  searchParams,
+}: {
+  searchParams: SearchParamsType;
+}) => {
+  const { q } = await searchParams;
+
+  const memebershipPlans = await db.membershipPlan.findMany({
+    where: {
+      ...(q
+        ? {
+            name: {
+              contains: q,
+              mode: "insensitive",
+            },
+          }
+        : {}),
+    },
+  });
   return (
     <div className="space-y-4">
       <PageHeader label="Membership Plans" actionUrl="/membership-plans/new" />
-      <Suspense fallback="">
-        <DataTable columns={columns} data={memebershipPlans} />
-      </Suspense>
+      <DataTable
+        columns={columns}
+        data={memebershipPlans}
+        showSearchInput
+        searchInputPlaceholder="Search Membership plan..."
+      />
     </div>
   );
 };
